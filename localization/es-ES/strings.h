710d7c3a7f6a9930816c79869e1e37d76d2ac7b2
Stefan Sundin <stefansundin@users.noreply.github.com>
2008-12-31 19:41:46 +0000

    Adding Spanish translation from Fabrizio Ferri. Thanks!

---
+++ localization/es-ES/strings.h
@@ -0,0 +1,41 @@
+/*
+	SuperF4 - es-ES localization by Fabrizio Ferri (algernon@gmail.com)
+	Do not localize APP_NAME, it will be automatically replaced.
+	Keep this file in UTF–8
+	
+	Copyright (C) 2008  Stefan Sundin (recover89@gmail.com)
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+*/
+
+//This will be updated when the strings in the program changes
+#define L10N_VERSION 1
+
+//Strings
+#define L10N_MENU_ENABLE       L"Activar"
+#define L10N_MENU_DISABLE      L"Desactivar"
+#define L10N_MENU_HIDE         L"Ocultar icono"
+#define L10N_MENU_AUTOSTART    L"Iniciar con Windows"
+#define L10N_MENU_UPDATE       L"Actualización disponible"
+#define L10N_MENU_ABOUT        L"Acerca de"
+#define L10N_MENU_EXIT         L"Salir"
+#define L10N_TRAY_ENABLED      APP_NAME " (activado)"
+#define L10N_TRAY_DISABLED     APP_NAME " (desactivado)"
+#define L10N_UPDATE_BALLOON    L"Nueva versión disponible"
+#define L10N_UPDATE_DIALOG     L"Hay una nueva versión disponible.\n¿Ir a la página web?"
+#define L10N_ABOUT_TITLE       L"Acerca de " APP_NAME
+#define L10N_ABOUT             APP_NAME " - " APP_VERSION "\n\
+Por Stefan Sundin\n\
+Traducido por Fabrizio Ferri (algernon@gmail.com)\n\
+\n\
+Presiona Control+Alt+F4 para terminar el proceso de la ventana seleccionada.\n\
+El efecto es el mismo que si mataras el proceso desde el Administrador de tareas.\n\
+\n\
+También puedes terminar un proceso pulsando la tecla Windows+F4 y luego pulsando la ventana con el ratón.\n\
+Pulsa Escape o el botón derecho para salir del modo sin terminar ningún programa.\n\
+\n\
+Visita la página web en "APP_URL"\n\
+Envía comentarios a recover89@gmail.com"
