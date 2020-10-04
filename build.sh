8f824c9549a072194e373bc34042a2c85f25c4f3
Stefan Sundin <git@stefansundin.com>
2020-10-04 14:05:43 -0700

    Clean up resource.

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 if [[ "$1" == "release" ]]; then
   mkdir -p bin/32 bin/64
 
-  i686-w64-mingw32-windres -o xkill.o include/xkill.rc
+  i686-w64-mingw32-windres -o xkill.o include/app.rc
   i686-w64-mingw32-gcc -o bin/32/xkill.exe xkill.c xkill.o -mwindows -O2 -s
 
-  x86_64-w64-mingw32-windres -o xkill.o include/xkill.rc
+  x86_64-w64-mingw32-windres -o xkill.o include/app.rc
   x86_64-w64-mingw32-gcc -o bin/64/xkill.exe xkill.c xkill.o -mwindows -O2 -s
 else
-  x86_64-w64-mingw32-windres -o xkill.o include/xkill.rc
+  x86_64-w64-mingw32-windres -o xkill.o include/app.rc
   x86_64-w64-mingw32-gcc -o xkill.exe xkill.c xkill.o -mwindows -O2 -g -DDEBUG
 fi
